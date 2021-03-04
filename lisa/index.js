ac9c47ec671f718caa3aade727b024c22518fbf4
lisamaliqi <isamaliqi@hotmail.com>
2021-03-04 15:02:08 +0100

    test

---
+++ lisa/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+
+const fetch = require('node-fetch');
+
+(async () => { 
+  const response = await fetch('https://trefle.io/api/v1/plants?token=RLDwZa0xCVzkqz0Ho4s_8IkuIy0bMSdwTTYJh64fNCY');
+  const json = await response.json();
+  console.log(json);
+})();
