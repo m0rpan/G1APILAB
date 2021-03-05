0ea1b15525ae2c31b07cd7d4b8037e14de392a8d
m0rpan <78790978+m0rpan@users.noreply.github.com>
2021-03-05 14:03:00 +0100

    got it working

--- Morris/Public/sketch.js
+++ Morris/Public/sketch.js
@@ -1,37 +1,101 @@
-getData();
+// EXAMPLE 1.
+getDataP_size();
 
-async function getData(){
-  const res = await fetch('/api/Plants', options);
+async function getDataP_size(){
+  const res = await fetch('/api/PlantsSize');
   const myData = await res.json();
   console.log(myData);
-  const options ={
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(myData)
-  }
-  showDataOnPage(myData);
+
+  showList(myData)
+
 }
 
-function showDataOnPage(data){
-  console.log(data);
+function showList(data){
   const dataDiv= document.getElementById("showData");
-  dataDiv.innerHTML= data;
+  const list = document.createElement("ul");
+
+  for (var i = 0; i < data.data.length; i++) {
+    const item = document.createElement("li");
+    item.appendChild(document.createTextNode(data.data[i].common_name));
+    list.appendChild(item);
+  }
+  dataDiv.appendChild(list);
+}
+
+
+
+
+
+
+//EXAMPLE 2
+getDataP_edible();
+
+
+async function getDataP_edible(){
+  const res = await fetch('/api/PlantsEdible');
+  const myDataEdible = await res.json();
+  console.log(myDataEdible);
+
+  showList_WithImg(myDataEdible)
 
 }
 
-/*const data = {
-  status: 'success',
-  data: 'Hello friend!',
+function showList_WithImg(data){
+  const dataDiv2= document.getElementById("showDataEdible");
+  const list = document.createElement("ul");
+
+  for (var i = 0; i < data.data.length; i++) {
+    // Creating elements
+    const item = document.createElement("li");
+    const nameElement = document.createElement("p");
+    const imgElement = document.createElement("img");
+    //setting width and hight to imgElement
+    imgElement.style.width = "150px";
+    imgElement.style.height = "200px";
+    
+    //if common_name does not exist print scientific_name
+    if(data.data[i].common_name){
+      nameElement.appendChild(document.createTextNode(data.data[i].common_name));
+    }
+    else{
+      nameElement.appendChild(document.createTextNode(data.data[i].scientific_name));
+    }
+    
+    //if imge_url does not exist print default img
+    if(data.data[i].image_url){
+      imgElement.src = data.data[i].image_url;
+    }
+    else{
+      imgElement.src= "https://media.giphy.com/media/SvoWj5IUsGwmHPTQqO/giphy.gif";
+      imgElement.style.width = "200px";
+    }
+    
+    //place nameElement & imgElement in item --> place item in list
+    item.appendChild(nameElement);
+    item.appendChild(imgElement);
+    list.appendChild(item);
+
+    getIndividualPlant(data.data[i].id);
+
+  }
+  //place list in div
+  dataDiv2.appendChild(list);
+
+
 }
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data)
+
+
+function getIndividualPlant(id){
+      //data that I want to send back to the api/database
+      const options = {
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(id)
+      }
+      fetch('/api/PlantID', options)
+      .then(resp => resp.json())
+      .then(data => console.log(data));
 }
-fetch('/api', options)
-.then(response => response.json())
-.then(data => console.log(data));*/
+
--- Morris/index.js
+++ Morris/index.js
@@ -5,18 +5,30 @@ app.listen(3000, ()=> console.log('listen at 3000'));
 app.use(express.static('public'));
 app.use(express.json( {limit: '1mb'}));
 
-app.post('/api', (request, response)=> {
-  console.log(request.body);
-  response.json({
-    status: 'the request was a success'
-  })
-});
-
-
-
-app.get('/api/Plants', (request,response)=>{
+//Example 1, get data about plants with hight between 5cm & 20cm
+app.get('/api/PlantsSize', (request,response)=>{
   fetch('https://trefle.io/api/v1/species?range%5Bmaximum_height_cm%5D=5%2C20&token=tPkth5TH6pRDS6z3Ug7wb3JPeH-veqxTjiRiYnZwDeY')
   .then(res => res.json())
   .then(data => response.json(data));
+})
+
+
+
+
+
+
+//Example 2 
+app.get('/api/PlantsEdible', (request,response)=>{
+  fetch('https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=tPkth5TH6pRDS6z3Ug7wb3JPeH-veqxTjiRiYnZwDeY')
+  .then(res => res.json())
+  .then(data => response.json(data));
+})
+
+
+app.post('/api/PlantID', (request, response) => {
+  console.log(request.body)
+ /* fetch('https://trefle.io/api/v1/species/'+request.id + '&token=tPkth5TH6pRDS6z3Ug7wb3JPeH-veqxTjiRiYnZwDeY')
+  .then(res => res.json())
+  .then(data => response.json(data));*/
 })
 
