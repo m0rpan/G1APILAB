dbbd6d27d9da0447a697601256787609a2632874
AlvaJL <78794467+AlvaJL@users.noreply.github.com>
2021-03-11 10:21:22 +0100

    Prototype finished and ReadMe files for Experiment 1&2 done

--- AlvasFiles/Public/Prototype/sketch.js
+++ AlvasFiles/Public/Prototype/sketch.js
@@ -1,6 +1,9 @@
 //Prototype
 getDataFromAPI();
 
+//variable for input value
+let inputTemp;
+
 
 async function getDataFromAPI(){
   const res = await fetch('/api/FilterOutNull');
@@ -12,32 +15,31 @@ async function getDataFromAPI(){
 }
 
 
-//function that makes the name & img data from API show on page
+//makes the name & img data from API show on page
 async function show_plant_name_Img(data){
   
   const listDiv= document.getElementById("Pick_a_Plant");
   
   
   for (var i = 0; i < data.data.length; i++) {
-    //calls api with the plant id
-    /*const individualData = await getIndividualInfo(data.data[i].id);*/
-    
-
     // Creating elements
-    
     const nameImgDiv = document.createElement("div");
     const nameElement = document.createElement("p");
     const imgElement = document.createElement("img");
-    /*const infoElement = document.createElement("div");
-    const minTempElement = document.createElement("p");
-    const maxTempElement = document.createElement("p");*/
+    
+    //gives an id attribue to nameImgDiv, id= id from plant array 
+    nameImgDiv.setAttribute("id",data.data[i].id);
 
     //setting width and hight to imgElement
     imgElement.style.width = "150px";
     imgElement.style.height = "200px";
 
+    // removes linebreak between divs' & puts a margent to the divs'
     nameImgDiv.style.display = "inline-block";
     nameImgDiv.style.margin = "15px";
+
+    //if div is clicked compareInput function will be called
+    nameImgDiv.addEventListener("click", compareInput);
     
     //if common_name does not exist print scientific_name
     if(data.data[i].common_name){
@@ -55,39 +57,57 @@ async function show_plant_name_Img(data){
       imgElement.src= "https://media.giphy.com/media/SvoWj5IUsGwmHPTQqO/giphy.gif";
       imgElement.style.width = "200px";
     }
-
-    //if minimun_temerature does not exist print "Info missing"
-    /*if(individualData.data.growth.minimum_temperature.deg_c){
-      minTempElement.appendChild(document.createTextNode('Min Temperature: ' + individualData.data.growth.minimum_temperature.deg_c+ '°C'+ ' || ')); 
-    }
-    else{
-      minTempElement.appendChild(document.createTextNode('Min Temperature: Info missing'+ ' || '));
-    }
-
-     //if maximum_temperature does not exist print "Info missing"
-    if(individualData.data.growth.maximum_temperature.deg_c){
-      minTempElement.appendChild(document.createTextNode('Max Temperature: ' + individualData.data.growth.maximum_temperature.deg_c+ '°C' ));
-    }
-    else{
-      minTempElement.appendChild(document.createTextNode('Max Temperature: Info missing'));
-    }*/
     
-    //place nameElement & imgElement in item --> place item in list
-    
+    //place nameElement & imgElement in nameImgDiv(div) --> place nameImgDiv in listDiv
     nameImgDiv.appendChild(nameElement);
     nameImgDiv.appendChild(imgElement);
-    /*item.appendChild(infoElement);
-    
-    // place info about plant in infoElemnet div
-    infoElement.appendChild(minTempElement);
-    infoElement.appendChild(maxTempElement);*/
-    
-    //place div in div
     listDiv.appendChild(nameImgDiv);
 
     
   }
-
-  
-  
 }
+
+//get temperature value from user
+function saveUserTemp(){
+  inputTemp = document.getElementById("temp").value;
+  document.getElementById("value").innerHTML= "Your Temperature: " + inputTemp;
+}
+
+async function compareInput() {
+  //if inputTemp has value = true
+  if(inputTemp != null){
+    //gets extended data that includes the minimum_temperature
+    const idData = await getExtendedDataFromAPI(this.id);
+    //gives diffrent messages in alert boxes depending on if the inputTemp is more than the minimum_temperature of the plant
+    if(idData.data.growth.minimum_temperature.deg_c <= inputTemp){
+      alert(`IT'S A MATCH!\n
+      Your Temp: ${inputTemp}
+      Plants Minimum Temperature: ${idData.data.growth.minimum_temperature.deg_c}`);
+    }else{
+      alert(`This plant has a minimum temperature that is lower than: ${inputTemp}\n\nPlants Minimum Temperature: ${idData.data.growth.minimum_temperature.deg_c}`);
+    }
+  }
+}
+
+//Sends id to retrive extended data from API that includes the minimum_temperature
+async function getExtendedDataFromAPI(id){
+  
+  const idObject = {id}
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(idObject)
+  }
+
+
+  return await fetch('/api/PlantID', options)
+  .then(resp => resp.json())
+  .then(data => {
+    return data;
+  });
+}
+
+
+
