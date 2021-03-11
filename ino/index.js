b2ad6bd479b290cfc2999b582a205bbd9602972c
Inotila <78792300+Inotila@users.noreply.github.com>
2021-03-12 00:06:35 +0100

    added express.json

--- ino/index.js
+++ ino/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json( {limit: '1mb'}));
 
 //example 1: trying to get the the height
 app.get('/api/plantheight', (request,response)=>{
