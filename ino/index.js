f29336bc6115a73ebea48b588b63fe29f8617297
Inotila <78792300+Inotila@users.noreply.github.com>
2021-03-11 22:03:38 +0100

    entering the example

--- ino/index.js
+++ ino/index.js
@@ -3,3 +3,10 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+
+//example 1: trying to get the the height
+app.get('/api/plantheight', (request,response)=>{
+    fetch('https://trefle.io/api/v1/species?range%5Bmaximum_height_cm%5D=5%2C20&token=Xz3zPU0yxTlTQgPCL65vqZQP98wTLL2ETe70d2MsmUE')
+    .then(res => res.json())
+    .then(data => response.json(data));
+  });
