560518f475a706bb611f8e12302cb20ebf22eb83
Inotila <78792300+Inotila@users.noreply.github.com>
2021-03-12 12:03:32 +0100

    struggle is real
    I added some touch ups on my work, readmes, comments in code, etc. still trying to get this to work as desired.

--- ino/index.js
+++ ino/index.js
@@ -5,14 +5,14 @@ app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json( {limit: '1mb'}));
 
-//example 1: trying to get the the height
+//example 1: trying to get the height
 app.get('/api/plantheight', (request,response)=>{
     fetch('https://trefle.io/api/v1/species?range%5Bmaximum_height_cm%5D=5%2C20&token=Xz3zPU0yxTlTQgPCL65vqZQP98wTLL2ETe70d2MsmUE')
     .then(res => res.json())
     .then(data => response.json(data));
   });
 
-  //example 2: trying to get the the height
+  //example 2: trying to getAntartica plants
 app.get('/api/Antartica plants', (request,response)=>{
     fetch('https://trefle.io/api/v1/distributions/antarctica/plants?token=Xz3zPU0yxTlTQgPCL65vqZQP98wTLL2ETe70d2MsmUE')
     .then(res => res.json())
