040ea1be996255ad3c9f92212c95320d62a2e94a
Inotila <78792300+Inotila@users.noreply.github.com>
2021-04-24 23:57:44 +0200

    installed node-fetch

--- ino/index.js
+++ ino/index.js
@@ -2,3 +2,5 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+const fetch = require('node-fetch');
+app.use(express.json( {limit: '1mb'}));
