24b966638bbe280d8b41b4b6244e528f90a5cb05
Inotila <78792300+Inotila@users.noreply.github.com>
2021-04-25 20:16:37 +0200

    created my example folders and my prototype

--- ino/index.js
+++ ino/index.js
@@ -6,11 +6,33 @@ app.use(express.static('public'));
 const fetch = require('node-fetch');
 app.use(express.json( {limit: '1mb'}));
 
+const apiToken = '0KQjJWrg0Rz3iVgO5oV4xjhqo_8VKAqkYFl17ObAV6M';
+const baseUrl = "https://trefle.io"
 //example 1
 app.get(`/api`, async (request, response) =>{
     
-    const api_url = "https://trefle.io/api/v1/plants?token=0KQjJWrg0Rz3iVgO5oV4xjhqo_8VKAqkYFl17ObAV6M";
+    const api_url = `${baseUrl}/api/v1/plants?token=0KQjJWrg0Rz3iVgO5oV4xjhqo_8VKAqkYFl17ObAV6M`;
     const fetch_response = await fetch(api_url)
     const json = await fetch_response.json();
     response.json(json);
+});
+
+//example 2
+app.get(`/search`, async (request, response) =>{
+    
+    const api_url = `${baseUrl}/api/v1/plants/search?token=${apiToken}&q=${request.query.searchText}`;
+    const fetch_response = await fetch(api_url)
+    const json = await fetch_response.json();
+    response.json(json);
+});
+
+//prototype
+app.get(`/prototype`, async (request, response) =>{
+    
+    const api_url = `${baseUrl}/api/v1/distributions?token=${apiToken}`;
+    const fetch_response = await fetch(api_url)
+    const json = await fetch_response.json();
+    response.json(json);
+
+    
 });
